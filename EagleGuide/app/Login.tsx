5484660c68b8ed6b443674f674224361d62b5de7
Andrea <149334136+Andouria@users.noreply.github.com>
2025-10-08 03:37:13 -0500

    Add Login.tsx
    Template for login

---
+++ EagleGuide/app/Login.tsx
@@ -0,0 +1,21 @@
+import { Stylesheet } from "react_native"; // need to download
+
+const Login: { // needs to figure out stack
+    // vars go in here
+
+    const handleLogin = async () => {
+        // needs to use API calls
+    }
+
+    return (
+        
+    )
+};
+
+const styles = StyleSheet.create({
+    text: {
+        // template
+    }
+});
+
+export default Login;
