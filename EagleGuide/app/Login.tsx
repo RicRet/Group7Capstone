902b61c4d077f51a6d1b7dac9b8a01a6bb988e24
Andrea <andie.nhle@gmail.com>
2025-10-13 03:40:59 +0000

    Add Login.tsx backbone

--- EagleGuide/app/Login.tsx
+++ EagleGuide/app/Login.tsx
@@ -1,23 +1,89 @@
-import React, {useState} from 'react';
-import {StyleSheet} from "react-native"; // need to download
 import axios from 'axios';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native"; // need to download
 
-const Login: { // needs to figure out stack
-    // vars go in here
+const Login = () => { // needs to figure out stack 
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
     const handleLogin = async () => {
-        // needs to use API calls
+            if (username && password) {
+        try {
+            const response = await axios.post('http://needsIP/api/users/login', { username, password });
+            if (response.data.user) {
+                // successful login
+            }
+        }
+        catch (error) {
+            // if user and pass do not successfully login
+        }
+        } else {
+            // if there is no user or pass
+        }
     }
 
     return (
-        
-    )
+        <View style={styles.container}>
+            <Text style={styles.title}>Login</Text>
+            
+            <TextInput
+                style={styles.input}
+                placeholder="Username"
+                value={username}
+                onChangeText={setUsername}
+            />
+            
+            <TextInput
+                style={styles.input}
+                placeholder="Password"
+                secureTextEntry={true}
+                value={password}
+                onChangeText={setPassword}
+            />
+            
+            <TouchableOpacity style={styles.button} onPress={handleLogin}>
+                <Text style={styles.buttonText}>Login</Text>
+            </TouchableOpacity>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-    text: {
-        // template
-    }
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        padding: 10,
+        backgroundColor: '#ffffffff',
+    },
+    title: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginBottom: 10,
+        color: '#333',
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#050000ff',
+        borderRadius: 10,
+        paddingHorizontal: 10,
+        marginBottom: 10,
+        backgroundColor: 'white',
+        fontSize: 10,
+    },
+    button: {
+        backgroundColor: '#04492cff',
+        padding: 10,
+        borderRadius: 10,
+        alignItems: 'center',
+        marginTop: 10,
+        marginBottom: 10, // Add space above the message
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 10,
+        fontWeight: 'bold',
+    },
 });
 
 export default Login;
