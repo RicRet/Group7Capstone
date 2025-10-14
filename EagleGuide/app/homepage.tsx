fdfa0ac094b85b27a26292fc546ae4c63b96959a
T7galaxy <tanismorgan1@gmail.com>
2025-10-14 01:36:18 +0000

    Added Buttons For add route and view route

--- EagleGuide/app/homepage.tsx
+++ EagleGuide/app/homepage.tsx
@@ -1,10 +1,38 @@
-import { Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import { Button, StyleSheet, Text, View } from "react-native";
 
-export default function Homepage() {
+export default function Home() {
+  const router = useRouter();
+
   return (
-    <View
-    >
-      <Text>Edit Hello to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome To UNT EagleGuide</Text>
+      <Button
+        title="Add Route (Placeholder)"
+        onPress={() => router.push("/addroute")}
+      />
+       <View style={{ height: 20 }} /> {/* Spacer */}
+       <Button
+        title="View Routes (Placeholder)"
+        onPress={() => router.push("/Viewroute")}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,                  
+    justifyContent: "flex-start", 
+    alignItems: "center",    
+    padding: 20,
+    backgroundColor: "green",
+  },
+  text: {
+    marginTop: 50,
+    marginBottom: 20,         
+    fontSize: 30,
+    textAlign: "center",
+    color: "white",
+  },
+});
