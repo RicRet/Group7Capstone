4df47e3a66d7c54c943cde2ee3402ff428c09ca1
T7galaxy <tanismorgan1@gmail.com>
2025-10-14 00:02:53 +0000

    Added homepage.tsx

---
+++ EagleGuide/app/homepage.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function Homepage() {
+  return (
+    <View
+    >
+      <Text>Edit Hello to edit this screen.</Text>
+    </View>
+  );
+}
