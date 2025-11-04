9e78e3f303990b3aac8d051e813bc304c1cfb0eb
T7galaxy <tanismorgan1@gmail.com>
2025-11-03 19:44:19 -0600

    Updates server.js

--- api/src/server.js
+++ api/src/server.js
@@ -1,7 +1,10 @@
+//loads env before anything else
+
 import dotenv from 'dotenv';
 dotenv.config({ path: './api/src/.env' });
 console.log('✅ Loaded .env');
 
+//loads other imports
 const { default: express } = await import('express');
 const { default: cors } = await import('cors');
 const { default: routes } = await import('./addrouteps.js');
@@ -13,7 +16,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = createApp();
 
-// Mount your router
+//mounts the addroutefunction
 app.use('/api', routes);
 
 app.listen(PORT, () => {
