6ef383d444603d28d81c1ce9598d8aead42939d1
T7galaxy <tanismorgan1@gmail.com>
2025-11-03 19:43:14 -0600

    Updated server to be compatible with addrouteps.js

--- api/src/routes/v1/index.js
+++ api/src/routes/v1/index.js
@@ -2,13 +2,12 @@ import { Router } from 'express';
 import auth from './auth.routes.js';
 import gis from './gis.routes.js';
 import health from './health.routes.js';
-import preferences from './preferences.routes.js';
 import users from './users.routes.js';
 
 const r = Router();
 r.use('/auth', auth);
 r.use('/users', users);
-r.use('/preferences', preferences);
+//r.use('/preferences', preferences);
 r.use('/gis', gis);
 r.use('/', health);
 export default r;
--- api/src/server.js
+++ api/src/server.js
@@ -2,12 +2,19 @@ import dotenv from 'dotenv';
 dotenv.config({ path: './api/src/.env' });
 console.log('✅ Loaded .env');
 
+const { default: express } = await import('express');
+const { default: cors } = await import('cors');
+const { default: routes } = await import('./addrouteps.js');
 const { createApp } = await import('./app.js');
+
 
 
 const PORT = process.env.PORT || 3000;
 
 const app = createApp();
+
+// Mount your router
+app.use('/api', routes);
 
 app.listen(PORT, () => {
   console.log(`API server listening on port ${PORT}`);
@@ -18,4 +25,3 @@ process.on('SIGINT', () => {
   console.log('Received SIGINT, shutting down');
   process.exit(0);
 });
-
